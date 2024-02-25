6b770dd9b08742ad101dee3e3dfaa783d4d78a8f
Fazbr <147539294+Fazbr@users.noreply.github.com>
2024-02-25 09:04:12 +0000

    10m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { images } from '../constants';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <section>
+      <header>
+      <div>
+        <img src={images.Logo} alt="Logo" />
+      </div>
+      <div>
+<ul>
+  <li>
+    <a href='/'Home></a>
+    </li>
+    <li>
+    <a href='/'Home></a>
+    </li>
+    <li>
+    <a href='/'Home></a>
+    </li>
+    <li>
+    <a href='/'Home></a>
+    </li>
+    <li>
+    <a href='/'Home></a>
+    </li>
+    <li>
+    <a href='/'Home></a>
+    </li>
+      </div>
+    </header>
+  </section>
+  );
+};
 
-export default Header
+export default Header;
