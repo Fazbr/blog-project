87ff7cb4c0a30afa96ebf075b544c99e7336bece
Fazbr <147539294+Fazbr@users.noreply.github.com>
2024-02-28 15:13:09 +0000

    9m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../constants";
 
-const navItemsInfo =[
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Pages"},
-  {name: "Pricing"},
-  {name: "Feq"},
-] 
+const navItemsInfo = [
+  { name: "Home" },
+  { name: "Articles" },
+  { name: "Pages" },
+  { name: "Pricing" },
+  { name: "Feq" },
+];
 
-
-const NavItem = ({name}) => {
+const NavItem = ({ name }) => {
   return (
     <li className="relative group">
       <a href="/" className="px-4 py-2">
@@ -24,17 +23,25 @@ const NavItem = ({name}) => {
 };
 
 const Header = () => {
+  const[navisVisible, setnavIsVisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) =>{
+      return !curState;
+    });
+  };
+  
   return (
     <section>
-      <header className="container mx-auto px-5  flex justify-between py-4 items-center">
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
-          <img src={images.Logo} alt="Logo" />
+          <img className="w-16" src={images.Logo} alt="Logo" />
         </div>
-        <div className="flex gap-x-9 items-center">
+        <div className={'flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:fixed-row top-0 bottom-0 -right-full lg:static gap-x-9 items-center'}>
           <ul className="flex gap-x-2 font-semibold">
-{navItemsInfo.map((item) => (
-  <NavItem key={item.name} name={item.name} />
-))}
+            {navItemsInfo.map((item) => (
+              <NavItem key={item.name} name={item.name} />
+            ))}
           </ul>
           <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
             Sign in
