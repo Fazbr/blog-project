702b3dc712890f285ae6e17e667de3f3e7b4e863
Fazbr <147539294+Fazbr@users.noreply.github.com>
2024-03-01 15:53:12 +0000

    responsev header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,86 @@
 import React, { useState } from "react";
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+
 import { images } from "../constants";
 
 const navItemsInfo = [
-  { name: "Home" },
-  { name: "Articles" },
-  { name: "Pages" },
-  { name: "Pricing" },
-  { name: "Feq" },
+  { name: "Home", type: "link" },
+  { name: "Articles", type: "link" },
+  { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
+  { name: "Pricing", type: "link" },
+  { name: "Faq", type: "link" },
 ];
 
-const NavItem = ({ name }) => {
+const NavItem = ({ item }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-        /
-      </span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <>
+          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+            <span>{item.name}</span>
+            <IoIosArrowDown />
+          </a>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => (
+                <a
+                  href="/"
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                >
+                  {page}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </li>
   );
 };
 
 const Header = () => {
-  const[navisVisible, setnavIsVisible] = useState(false);
+  const [navIsVisible, setNavIsVisible] = useState(false);
 
   const navVisibilityHandler = () => {
-    setNavIsVisible((curState) =>{
+    setNavIsVisible((curState) => {
       return !curState;
     });
   };
-  
+
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-16" src={images.Logo} alt="Logo" />
         </div>
-        <div className={'flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:fixed-row top-0 bottom-0 -right-full lg:static gap-x-9 items-center'}>
-          <ul className="flex gap-x-2 font-semibold">
+        <div className=" lg:hidden z-50">
+          {navIsVisible ? (
+            <IoCloseSharp className="w-6 h-6" onClick={navVisibilityHandler} />
+          ) : (
+            <IoMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+          )}
+        </div>
+        <div
+          className={`${
+            navIsVisible ? "right-0" : "-right-full"
+          } transition-all duration-300 mt-[56px] lg:mg-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:fixed-row top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemsInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
-          <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
             Sign in
           </button>
         </div>
