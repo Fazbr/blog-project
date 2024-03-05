9ce963df9e773a49cb12af3129764c19ef520315
Fazbr <147539294+Fazbr@users.noreply.github.com>
2024-03-05 17:43:17 +0000

    CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,15 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
+
 
 const images = {
   Logo,
   HeroImage,
   post1Image,
   PostProfileImage,
+  CtaImage,
 };
 
 export default images;
